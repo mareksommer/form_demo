7896e9cb765c64ae92e4d98ebff79b8b0ed76b5b
Marek Sommer <marek.sommer.1@gmail.com>
2025-01-31 10:16:20 +0100

    Add error list component

---
+++ src/components/ErrorList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul :id="id" class="error-list">
+    <li v-for="error in errors" :key="error" class="error-list-item">{{ error }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  errors?: string[]
+  id?: string
+}
+
+const props = defineProps<Props>()
+const id = props.id || 'error-list'
+</script>
+
+<style scoped>
+.error-list {
+  list-style-type: none;
+  padding: 0;
+}
+.error-list-item {
+  color: red;
+}
+</style>
